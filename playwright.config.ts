d900bd492180a2156ca16574110289c3f23de040
Dione Araujo Quevedo <dionequevedo.avanade@natura.net>
2026-07-10 23:29:55 -0300

    Correção de teste e de configurações

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -30,10 +32,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    actionTimeout: 3000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL: process.env.BASE_URL || 'https://loginxp.vercel.app',
+    baseURL: "https://loginx.vercel.app"
   },
 
   /* Configure projects for major browsers */
@@ -47,7 +49,15 @@ export default defineConfig({
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
     // {
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
