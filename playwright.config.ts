3dd785b6c52cb94527912eeeb8b5c6d40145b4f4
Dione Araujo Quevedo <dionequevedo.avanade@natura.net>
2026-08-07 14:47:19 -0300

    feat: enable mobile and branded browser configurations in Playwright test suite

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,7 +58,7 @@ export default defineConfig({
 
 
     /* Test against mobile viewports. */
-/*    
+
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
@@ -66,18 +66,19 @@ export default defineConfig({
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
-    }, 
-*/
+    },
+
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
